0ed2e839afd6de74f4bcc1d83e0391347a3b3e38
simonsimcity <simonsimcity@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-11-19 12:44:12 +0000

    Released new version in TER (1.1.6) - Fix problem if tt_news contains sub-content in it's body
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/indexedsearch_ttnews_crawler/trunk@86909 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,49 +3,46 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "indexedsearch_ttnews_crawler".
  *
- * Auto generated 29-05-2013 07:26
+ * Auto generated 19-11-2014 12:33
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'IndexedSearch Crawler for tt_news',
 	'description' => 'Adds a crawler for tt_news entries into the indexed_search configuration',
 	'category' => 'misc',
-	'shy' => 0,
-	'version' => '1.1.4',
-	'dependencies' => 'indexed_search,crawler,tt_news',
+	'shy' => false,
+	'version' => '1.1.6',
+	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
 	'state' => 'stable',
-	'uploadfolder' => 0,
+	'uploadfolder' => false,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearcacheonload' => 0,
+	'clearcacheonload' => false,
 	'lockType' => '',
 	'author' => 'Simon Schick',
 	'author_email' => 'simonsimcity@gmail.com',
 	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '4.5.0-6.1.99',
-			'indexed_search' => '',
-			'crawler' => '',
-			'tt_news' => '',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
 		),
-		'conflicts' => array(
+		'conflicts' => 
+		array (
 		),
-		'suggests' => array(
+		'suggests' => 
+		array (
 		),
-	),
-	'_md5_values_when_last_written' => 'a:8:{s:12:"ext_icon.gif";s:4:"c96c";s:17:"ext_localconf.php";s:4:"0c9b";s:14:"ext_tables.php";s:4:"995c";s:14:"ext_tables.sql";s:4:"1219";s:17:"locallang_tca.xml";s:4:"0713";s:14:"doc/manual.pdf";s:4:"4122";s:14:"doc/manual.sxw";s:4:"a420";s:47:"hooks/class.tx_indexedsearch_ttnews_crawler.php";s:4:"115f";}',
-	'suggests' => array(
 	),
 );
 
